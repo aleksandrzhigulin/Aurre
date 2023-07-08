e6a50a8103b7cc964ffd62b32eec6b8cc364552b
payl0adK <sanyarfx@yandex.ru>
2023-07-08 15:59:50 +0300

    Redesign

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,77 @@
 <template>
-    <div>
-        <div class="posts">
-            <div class="post" v-for="post in posts" :key="post.id">
-                <p class="title"><strong>{{ post.title }}</strong></p>
-                <p class="preview">{{ post.content }}</p>
-                <p class="author">{{ post.author }}</p>
+    <header>
+        <div class="container">
+            <div class="pages">
+                <a href="">Home</a>
             </div>
-
+            <div class="user">
+                <div class="user__actions">
+                    <a href="">Write a post</a>
+                </div>
+                <div class="user__info">
+                    <div class="user__avatar">
+                        <img src="" alt="">
+                    </div>
+                    <p>Ancapybara</p>
+                </div>
+            </div>
         </div>
-    </div>
-    <form @submit.prevent>
-        <h2>New post:</h2>
-        <input 
-        v-bind:value="title" 
-        @input="title = $event.target.value"
-        type="text" 
-        placeholder="Title:"
-        >
-        
-        <input 
-        v-bind:value="preview" 
-        @input="preview = $event.target.value"
-        type="text" 
-        placeholder="Content:"
-        >
+    </header>
+    <div class="container">
+        <div>
+            <div class="posts">
+                <div class="post" v-for="post in posts" :key="post.id">
+                    <div class="post__header">
+                       <p class="title">{{ post.title }}</p> 
+                    </div>
+                    <div class="post__body">
+                        <div class="post__image">
+                            <img src="./image/newyork.jpg" alt=""> 
+                        </div> 
+                    </div>
+                    <div class="post__footer">
+                        <div class="post__info">
+                            <div class="post__info-rating">
+                                <i class="fa-regular fa-heart"></i>
+                                <span>+200</span>
+                            </div>
 
-        <input 
-        v-bind:value="author" 
-        @input="author = $event.target.value"
-        type="text" 
-        placeholder="Author:"
-        >
-        <button @click="createPost">Create</button>
-    </form>
+                            <div class="post__info-author">
+                                <p class="author">{{ post.author }}</p> 
+                             </div>
+                        </div>
+                    </div>
+                    
+                </div>
+    
+            </div>
+        </div>
+        <form @submit.prevent>
+            <h2>New post:</h2>
+            <input class="input_default"
+            v-bind:value="title" 
+            @input="title = $event.target.value"
+            type="text" 
+            placeholder="Title:"
+            >
+            
+            <input class="input_default"
+            v-bind:value="preview" 
+            @input="preview = $event.target.value"
+            type="text" 
+            placeholder="Content:"
+            >
+    
+            <input class="input_default"
+            v-bind:value="author" 
+            @input="author = $event.target.value"
+            type="text" 
+            placeholder="Author:"
+            >
+            <button @click="createPost" class="yellow_btn">Create new post</button>
+        </form>
+    </div>
+
 </template>
 
 <script>
@@ -80,39 +119,5 @@ export default {
 </script>
 
 <style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-form {
-    width: 50%;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    width: 30rem;
-    
-}
-form * {
-    margin-bottom: 1em;
-    height: 3em;
-}
-form h2 {
-    margin-bottom: 0;
-    height: 100%;
-}
-.posts {
-    width: 50%;
-    margin: 0 auto;
-    background-color: #FFFFFF;
-    border: 3px solid black;
-    border-radius: 15px;
-    padding: 10px;
-}
-.post {
-    margin-bottom: 5em;
-}
-.post:last-child {
-    margin-bottom: 0;
-}
+@import 'style/main.css';
 </style>
