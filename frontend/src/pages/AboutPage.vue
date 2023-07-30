5087547ec632e346f7394c6a6e33d31637b7ace2
payl0adK <sanyarfx@yandex.ru>
2023-07-30 16:38:58 +0300

    about page update

--- frontend/src/pages/AboutPage.vue
+++ frontend/src/pages/AboutPage.vue
@@ -5,7 +5,24 @@
 <template>
   <div class="container">
     <div class="about">
-
+      <div class="about__avatar">
+        <img src="http://localhost:8080/files/get/a67661f3-5ea8-488e-bcde-bdeb8b140d88ancat.jpg" alt="">
+      </div>
+      <div class="about__info">
+        <h1 class="name">Aleksandr Zhigulin</h1>
+        <div class="links">
+          <a href="https://github.com/payl0adk" target="_blank">Github</a>
+          <a href="" target="_blank">Linkedin</a>
+          <a href="https://t.me/fleasc" target="_blank">Telegram</a>
+        </div>
+        <div class="about-text">
+          <p class="header">About me</p>
+          <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Aperiam architecto blanditiis consectetur deserunt dolore expedita impedit labore magni,
+            molestias quam quod repellendus similique sunt ullam voluptate.
+            Illo necessitatibus obcaecati placeat?</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
