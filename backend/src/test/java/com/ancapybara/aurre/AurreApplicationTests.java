8719fa1c6333f1b8c1ba156386d73aa3da8b9b4d
payl0adK <sanyarfx@yandex.ru>
2023-08-13 15:02:54 +0300

    File exists test

--- backend/src/test/java/com/ancapybara/aurre/AurreApplicationTests.java
+++ backend/src/test/java/com/ancapybara/aurre/AurreApplicationTests.java
@@ -1,9 +1,9 @@
 package com.ancapybara.aurre;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class AurreApplicationTests {
 
 	@Test
